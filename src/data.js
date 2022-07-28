61791e6eccdcb543541f616a967caa2314929359
AnvitaLingampalli <lingampalligunny@gmail.com>
2022-07-28 23:27:08 +0530

    updated data.js

--- src/data.js
+++ src/data.js
@@ -1,38 +1,38 @@
 export const projects = [
     {
-      title: "React Reserve",
-      subtitle: "MERN Stack",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./doctorPortal.png",
-      link: "https://reactbootcamp.com",
+      title: "Coming soon...",
+      // subtitle: "MERN Stack",
+      // description:
+      //   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      // image: "./doctorPortal.png",
+      // link: "https://reactbootcamp.com",
     },
     {
-      title: "React Tracks",
-      subtitle: "React and Python",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./asu.png",
-      link: "https://reedbarger.com",
+      title: "Coming soon...",
+      // subtitle: "React and Python",
+      // description:
+      //   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      // image: "./asu.png",
+      // link: "https://reedbarger.com",
     },
     {
-      title: "DevChat",
-      subtitle: "React and Firebase",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./myPic.jpg",
-      link: "https://jsbootcamp.com",
+      title: "Coming soon...",
+      // subtitle: "React and Firebase",
+      // description:
+      //   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      // image: "./myPic.jpg",
+      // link: "https://jsbootcamp.com",
     },
     {
-      title: "Epic Todo App",
-      subtitle: "React Hooks",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-4.gif",
-      link: "https://pythonbootcamp.com",
+      title: "Coming soon...",
+      // subtitle: "React Hooks",
+      // description:
+      //   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      // image: "./project-4.gif",
+      // link: "https://pythonbootcamp.com",
     },
   ];
 
   export const skills = [
-    "JavaScript", "React", "Java", "HTML", "CSS", "C++"
+    "JavaScript", "React", "Java", "HTML", "CSS", "C++", "C", "JavaFX" 
   ];
