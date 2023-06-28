2466a0967dd5cb2ab4b49aa2f9804fbbeffa2109
AnvitaLingampalli <lingampalligunny@gmail.com>
2023-06-28 13:37:49 -0700

    Added projects

--- src/data.js
+++ src/data.js
@@ -1,35 +1,32 @@
 export const projects = [
     {
-      title: "Coming soon...",
-      // subtitle: "MERN Stack",
-      // description:
-      //   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      // image: "./doctorPortal.png",
-      // link: "https://reactbootcamp.com",
+      title: "Daydule",
+      subtitle: "A Capstone Project",
+      description:
+      "Worked with a company, ShadyForge LLC, as a part of ASU senior year capstone project. Developed an all-in-one calendar and scheduling application for event planners. Collaborated with the backend team to implement the architecture using PostgreSQL database for efficient data storage and retrieval.",
+      image: "./Daydule.gif",
     },
     {
-      title: "Coming soon...",
-      // subtitle: "React and Python",
-      // description:
-      //   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      // image: "./asu.png",
-      // link: "https://reedbarger.com",
+      title: "BookshelfPro",
+      subtitle: "an iOS App",
+      description:
+       "BookShelfPro is a books app which allows the user to browse through the best selling books of the week. Users can find more details about the book by clicking on it. They can also add books to their wishlist to keep track of their interests. The app also has a map feature to help users find bookstores nearby.",
+      image: "./BooksApp.gif",
+      link: "https://github.com/AnvitaLingampalli/BookShelfPro",
     },
     {
-      title: "Coming soon...",
-      // subtitle: "React and Firebase",
-      // description:
-      //   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      // image: "./myPic.jpg",
-      // link: "https://jsbootcamp.com",
+      title: "Riposte",
+      subtitle: "Android App",
+      description:
+         "Designed and developed an app which allows users to respond to exciting daily prompts in the form of multimedia articles. Users can upload audio recordings, images, or short responses based on the day's prompt.",
+      image: "./Riposte.gif",
     },
     {
-      title: "Coming soon...",
-      // subtitle: "React Hooks",
-      // description:
-      //   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      // image: "./project-4.gif",
-      // link: "https://pythonbootcamp.com",
+      title: "Pediatric Automation Tool",
+      subtitle: "A Web Application",
+      description:
+         "The Pediatric Automation Tool is designed and developed to provide users and medical offices with ability to view, edit, and store patient records via a web app. This software system is designed to reduce the paperwork and increase the accessibility of patient records.",
+      image: "./PAT.png",
     },
   ];
 
